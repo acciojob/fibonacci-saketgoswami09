d00c0e31d8f613f330abac666ddf755ecaf962a0
SAKET GIRI GOSWAMI <110333651+saketgoswami09@users.noreply.github.com>
2024-04-08 23:57:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function fibonacci(num) {
-// your code here
+    if (num === 0) {
+        return 0;
+    } else if (num === 1) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    }
 }
-
 module.exports = fibonacci;
