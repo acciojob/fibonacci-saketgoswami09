5c46e643eb03201ec973c8298eb72de788330f44
SAKET GIRI GOSWAMI <110333651+saketgoswami09@users.noreply.github.com>
2024-04-09 23:39:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 function fibonacci(num) {
-    if (num === 0) {
-        return 0;
+    if (num <= 0) {
+        return "Bhai, sirf positive integers allow hain!";
     } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
         return 1;
     } else {
-        return fibonacci(num - 1) + fibonacci(num - 2);
+        let fib = [0, 1];
+        for (let i = 2; i < num; i++) {
+            fib.push(fib[fib.length - 1] + fib[fib.length - 2]);
+        }
+        return fib[fib.length - 1];
     }
 }
 module.exports = fibonacci;
